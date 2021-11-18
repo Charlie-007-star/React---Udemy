6ed7394d80832df83406457b307887d9f5002dda
Frankenstein <frankenstein@pop-os.localdomain>
2021-11-18 11:24:12 +0530

    learning react styling

--- src/App.js
+++ src/App.js
@@ -4,17 +4,38 @@ const fName = "Sujin";
 const lName = "S R";
 const year = new Date().getFullYear();
 
+const customStyle = {
+    color:"blue",
+    fontSize: "50px",
+    border: "2px solid black"
+}
+
+var day = "Good Morning"
+var date = new Date();
+var hour = date.getHours();
+
+if(hour < 12){
+
+    day = "Good Morning"
+    customStyle.color = "red"
+}else if(hour < 18){
+
+    day = "Good Afternoon"
+    customStyle.color = "green"
+}else{
+
+    day = "Good Evening"
+    customStyle.color = "blue"
+}
+
+
 const App = () => {
     return (
         <>
         <h1 className="heading" contentEditable="true" spellCheck="false">Created By {`${fName} ${lName}`}!</h1>
         <p>Copyright:{year}</p>
-        <h1>My Fav Foods</h1>
-        <ul className="heading">
-            <li>Bacon</li>
-            <li>Steak</li>
-            <li>Shawarma</li>
-        </ul>
+        <h1 style={customStyle}>{day}</h1>
+
         </>
     )
 }
