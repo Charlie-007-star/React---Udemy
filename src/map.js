919499fd1dd8abab625099d8295761ecaca021df
Charlie-007-star <sujinsr2457@gmail.com>
2021-11-30 13:58:25 +0530

    find and find index and map and filter finished

---
+++ src/map.js
@@ -0,0 +1,59 @@
+let numbers = [3, 56, 2, 48, 5];
+
+/* ----------------------------------- map ---------------------------------- */
+// function double(x) {
+//   return x * 2;
+// }
+
+// const newNumbers = numbers.map(double);
+
+// console.log(newNumbers);
+
+
+/* -------------------------------- for each -------------------------------- */
+
+// var newNumbers = [];
+
+// numbers.forEach(function (x) {
+
+// newNumbers.push(x * 2);
+// });
+
+// console.log(newNumbers);
+
+/* --------------------------------- Filter --------------------------------- */
+
+// const newNumbers = numbers.filter(function (num){
+
+//   return num > 10;
+// })
+
+// console.log(newNumbers);
+
+
+/* --------------------------------- Reduce --------------------------------- */
+
+// var reduce = numbers.reduce(function (accumulator, currentValue) {
+
+//   return accumulator + currentValue;
+  
+// })
+
+// console.log(reduce);
+
+/* ---------------------------------- Find ---------------------------------- */
+
+// let find = numbers.find(function (num){
+//   return num > 10;
+// })
+
+// console.log(find);
+
+/* -------------------------------- FindIndex ------------------------------- */
+
+// let findIndex = numbers.findIndex(function (num){
+//   return num > 10;
+// })
+
+// console.log(findIndex);
+
